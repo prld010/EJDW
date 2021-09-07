7a847aec931b53d87c2148895d4401ef99b5fa55
prld010 <laurapera37@gmail.com>
2021-09-07 10:36:20 +0200

    Actualizado 07/09/21

--- HTML/19_Funcbusqueda.js
+++ HTML/19_Funcbusqueda.js
@@ -1,1 +1,7 @@
 document.write("<h1>Funciones de busqueda</h1>")
+
+var cadena= "Cuando cuentas cuentos cuenta cuantos cuentas porque ni no cuentas cuantos cuentos cuentas no sabras cuantos cuentos contastes"
+var busqueda= cadena.indexOf("cuentos")
+busqueda= cadena.lastIndexOf("cuentos")
+busqueda= cadena,Match("/cuentos/gi")
+console.log(busqueda);
